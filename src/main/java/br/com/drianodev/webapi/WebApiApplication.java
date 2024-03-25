ccb17fb4ca71d6db3c894cefc33a6756f01b2050
drianodev <adrianomendes661@gmail.com>
2024-03-25 09:02:41 -0300

    first commit

---
+++ src/main/java/br/com/drianodev/webapi/WebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.drianodev.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiApplication.class, args);
+	}
+
+}
